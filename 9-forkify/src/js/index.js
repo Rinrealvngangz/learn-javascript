b14c2f02a79c1f5078ed8ef9a86e8a64b59bd1d6
Rin <tuannmctk42@gmail.com>
2019-10-30 10:37:01 +0700

    Add file forkify

---
+++ 9-forkify/src/js/index.js
@@ -0,0 +1,5 @@
+// Global app controller
+
+import num from './test.js';
+
+console.log(`I imported ${num} from another module called test ! `);
---
+++ 9-forkify/src/js/test.js
@@ -0,0 +1,3 @@
+console.log('Imported modules');
+
+export default 123;
